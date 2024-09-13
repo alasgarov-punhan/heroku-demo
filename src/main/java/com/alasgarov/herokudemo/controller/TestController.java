35225ef3d43613788655251dd35ab87908231e85
punhan-alasgarov <punhan.alasgarov@sinam.net>
2024-09-14 02:33:31 +0400

    first commit

---
+++ src/main/java/com/alasgarov/herokudemo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.alasgarov.herokudemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "hi";
+    }
+}
